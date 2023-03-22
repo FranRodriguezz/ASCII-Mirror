0d2206979b2a080540a14e5641bc3a220933e567
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2023-03-22 10:06:35 -0300

    Modifications

---
+++ spring-boot-example/src/main/java/com/springbootExample/Main.java
@@ -0,0 +1,30 @@
+package com.springbootExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        return new GreetResponse("Hello", List.of("Java", "C++", "C"), new Person("Fran"));
+    }
+
+    record Person (String name){
+
+    }
+
+    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person){
+    }
+
+}
