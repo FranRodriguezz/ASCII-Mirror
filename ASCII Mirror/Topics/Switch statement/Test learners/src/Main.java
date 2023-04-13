525794164635c4c87ca19389e514311319479647
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2023-04-13 10:56:23 -0300

    ASCII Mirror

--- ASCII Mirror/Topics/Switch statement/Test learners/src/Main.java
+++ ASCII Mirror/Topics/Switch statement/Test learners/src/Main.java
@@ -4,5 +4,15 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // start coding here
+        int a = scanner.nextInt();
+        if (a == 1){
+            System.out.println("Yes!");
+        }
+        else if (a > 4){
+            System.out.println("Unknown number");
+        }
+        else{
+            System.out.println("No!");
+        }
     }
 }
