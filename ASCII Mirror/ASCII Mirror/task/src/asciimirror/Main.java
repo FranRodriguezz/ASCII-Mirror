4400720b2356b020e5b7b51c715f0afb11681f7a
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2023-04-13 11:00:41 -0300

    ASCII Mirror

---
+++ ASCII Mirror/ASCII Mirror/task/src/asciimirror/Main.java
@@ -0,0 +1,20 @@
+package asciimirror;
+
+import java.io.*;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        System.out.println("Input the file path:\n");
+        File file = new File(scanner.nextLine());
+        try (Scanner fileScanner = new Scanner(file)) {
+            while (fileScanner.hasNext()) {
+                System.out.println(fileScanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
+}
